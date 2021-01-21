f456f769269ae4d5269436139225fe67ba551413
Michelly Hiratsuka Zorzi <michelly.hiratsuka@gmail.com>
2021-01-21 20:37:01 -0300

    Exercícios da primeira aula

---
+++ js/principal.js
@@ -0,0 +1,4 @@
+var titulo = document.querySelector(".titulo"); //seleciona a classe que será trabalhada
+
+titulo.textContent = "Aparecida Nutricionista"; //muda o conteúdo da classe selecionada no comando acima
+
